066a585647eb4a3598cdd9a4d59a8fa4946ce874
footleg <drfootleg@gmail.com>
2021-02-15 17:28:39 +0000

    Added static pixels and change of gravity direction

--- examples/TeensyToZeroUnicornHATHD/ToZero_UnicornHATHD_Sand/ToZero_UnicornHATHD_Sand.ino
+++ examples/TeensyToZeroUnicornHATHD/ToZero_UnicornHATHD_Sand/ToZero_UnicornHATHD_Sand.ino
@@ -31,12 +31,29 @@
 // using the syntax *this
 class Animation : public RGBMatrixRenderer {
     public:
-        Animation(uint16_t width, uint16_t height, uint16_t shake)
-            : RGBMatrixRenderer{width,height}, animSand(*this,shake)
+        Animation(uint16_t width, uint16_t height, uint16_t shake, int16_t accel)
+            : RGBMatrixRenderer{width,height}, animSand(*this,shake), accel(accel)
         {
-            //Set up falling sand grains and static pixels
-            for (int x=0; x<5; x++){
-                for (int y=0; y<5; y++){
+            /* Consider the coordinate space mapping to the display. In this example class we have
+             * written the setPixel method such that position (0,0) is the bottom left of the LED
+             * matrix. So increasing X is going to the right across the display and increasing Y is 
+             * up the display. This means that setting acceleration to 0,100 will move the sand 
+             * grains up the display, and setting it to 100,0 will move them to the right of the 
+             * display.
+             */
+
+            //Draw static open box in the middle of the display
+            for (int x=5; x<11; x++){
+                setPixelColour(x,5,RGB_colour{255,0,255});
+            }
+            for (int y=6; y<11; y++){
+                setPixelColour(5,y,RGB_colour{255,0,255});
+                setPixelColour(10,y,RGB_colour{255,0,255});
+            }
+            
+            //Set up falling sand grains in 4 coloured blocks in the corners of the display
+            for (int x=0; x<4; x++){
+                for (int y=0; y<4; y++){
                     animSand.addGrain(x,y+11,RGB_colour{255,255,0});
                     animSand.addGrain(x+11,y+11,RGB_colour{0,255,255});
                     animSand.addGrain(x+11,y,RGB_colour{255,0,0});
@@ -52,8 +69,59 @@ class Animation : public RGBMatrixRenderer {
             unicorn = unicornHAT;
         }
         void animationStep() {
-            
+            int ax = 0;
+            int ay = 0;
+
+            //Run one animation cycle
             animSand.runCycle();
+
+            //Change gravity direction every 300 cycles
+            counter++;
+            if (counter > 300) {
+                counter = 0;
+                //Update gravity direction
+                switch (direction) {
+                    case 0:
+                        ax = 0;
+                        ay = 0;
+                        break;
+                    case 1:
+                        ax = accel;
+                        ay = 0;
+                        break;
+                    case 2:
+                        ax = 0;
+                        ay = -accel;
+                        break;
+                    case 3:
+                        ax = -accel;
+                        ay = 0;
+                        break;
+                    case 4:
+                        ax = 0;
+                        ay = accel;
+                        break;
+                    case 5:
+                        ax = accel;
+                        ay = -accel;
+                        break;
+                    case 6:
+                        ax = -accel;
+                        ay = -accel;
+                        break;
+                    case 7:
+                        ax = -accel;
+                        ay = accel;
+                        break;
+                    case 8:
+                        ax = accel;
+                        ay = accel;
+                        direction = -1;
+                        break;
+                }
+                animSand.setAcceleration(ax,ay);
+                direction++;
+            }
         }
 
         virtual void showPixels() {
@@ -75,14 +143,16 @@ class Animation : public RGBMatrixRenderer {
     private:
         UnicornHD unicorn;
         FallingSand animSand;
-
+        int counter = 0;
+        int direction = 0;
+        int16_t  accel;
         virtual void setPixel(uint16_t x, uint16_t y, RGB_colour colour) 
         {
             CRGB col;
             col.red = colour.r;
             col.green = colour.g;
             col.blue = colour.b;
-            unicorn.Pixels()[y*getGridHeight()+x] = col;
+            unicorn.Pixels()[(getGridHeight()-y-1)*getGridHeight()+x] = col;
         }
 };
 
@@ -91,7 +161,7 @@ static const unsigned long UNICORN_BRIGHTNESS = 128;
 
 /***** Global Variables *****/
 UnicornHD unicorn;
-Animation animation(16,16,10);
+Animation animation(16,16,10,100);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // SETUP
@@ -121,7 +191,7 @@ void loop()
     //Call animation class method to run a cycle
     animation.animationStep();
     //SHOULD USE FPS delay timing here
-    delay(20);
+    //delay(20);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
